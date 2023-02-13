ef3d9bc09ff37ffefb78e534ef4ce87b6b8068b3
Mychal Culpepper <mychalculpepper@Mychals-MBP.lan>
2023-02-12 21:15:05 -0600

    OOP exercises

---
+++ geometry.py
@@ -0,0 +1,7 @@
+import math
+
+def area_of_circle(radius):
+    return math.pi * (radius ** 2)
+
+def hypotenuse(side1, side2):
+    return math.sqrt(side1 ** 2 + side2 ** 2)
---
+++ math_exercises.py
@@ -0,0 +1,8 @@
+import geometry
+
+radius = 7
+print("Area of circle with radius", radius, "is:", geometry.area_of_circle(radius))
+
+side1 = 3
+side2 = 4
+print("Hypotenuse of a right angle triangle with sides", side1, "and", side2, "is:", geometry.hypotenuse(side1, side2))
